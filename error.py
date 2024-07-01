01d8bf6e8394bef82794c19a542618c4acb35b5c
staruser143 <seriesfan.in@gmail.com>
2024-07-01 21:10:49 +0530

    Create error.py

---
+++ error.py
@@ -0,0 +1,6 @@
+
+ {'name': 'Revoke', 'id': 'revoke', 'type': 'boolean'}
+
+Invalid argument `columns[2].type` passed into DataTable with ID "permissions-table".
+Expected one of ["any","numeric","text","datetime"]
+
