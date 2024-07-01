203c1c8272109d4ed1f8d075dcf1ee9b274380f9
staruser143 <seriesfan.in@gmail.com>
2024-07-01 21:21:09 +0530

    Create newerror.py

---
+++ newerror.py
@@ -0,0 +1,29 @@
+In the callback for output(s):
+  permissions-table.data
+  permissions-data.data
+  table-dropdown.options
+Output 0 (permissions-table.data) is already in use.
+To resolve this, set `allow_duplicate=True` on
+duplicate outputs, or combine the outputs into
+one callback function, distinguishing the trigger
+by using `dash.callback_context` if necessary.
+
+In the callback for output(s):
+  permissions-table.data
+  permissions-data.data
+  table-dropdown.options
+Output 1 (permissions-data.data) is already in use.
+To resolve this, set `allow_duplicate=True` on
+duplicate outputs, or combine the outputs into
+one callback function, distinguishing the trigger
+by using `dash.callback_context` if necessary
+
+In the callback for output(s):
+  permissions-table.data
+  permissions-data.data
+  table-dropdown.options
+Output 2 (table-dropdown.options) is already in use.
+To resolve this, set `allow_duplicate=True` on
+duplicate outputs, or combine the outputs into
+one callback function, distinguishing the trigger
+by using `dash.callback_context` if necessary.
